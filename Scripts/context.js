ad218fb6b926a5ef1d84e18abaac3eaac8c50dc9
Andrew Wenzloff <255278335+andificus@users.noreply.github.com>
2026-08-01 18:59:27 -0500

    Simplify context modifier function
    Refactor context modifier function for clarity.

--- Scripts/context.js
+++ Scripts/context.js
@@ -1,8 +1,7 @@
 // Your "Context" tab should look like this
+InnerSelf("context");
 const modifier = (text) => {
-  // Your other context modifier scripts go here (preferred)
-  [text, stop] = AutoCards("context", text, stop);
-  // Your other context modifier scripts go here (alternative)
-  return {text, stop};
+  // Any other context modifier scripts can go here
+  return { text, stop };
 };
 modifier(text);
